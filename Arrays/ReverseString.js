fa44c54e20b39efb22565e92c4d2cda85f6b7662
Aks-20 <akshatgupta0043@gmail.com>
2024-09-15 22:58:10 +0530

    reverse

---
+++ Arrays/ReverseString.js
@@ -0,0 +1,8 @@
+//Convert the String to Array;(Split method)
+//Reverse the array(reverse method)
+//Convert the array back to string;(join method)
+const reverseString =(str)=>
+str.split("").reverse().join()
+console.log(reverseString("hello"));
+
+

103054dbc3ebb8f77746b1c51770ad425c6d2d9b
Aks-20 <akshatgupta0043@gmail.com>
2024-09-16 00:11:27 +0530

    commit

--- Arrays/ReverseString.js
+++ Arrays/ReverseString.js
@@ -6,3 +6,15 @@ str.split("").reverse().join()
 console.log(reverseString("hello"));
 
 
+function reverseString1(str) {
+    let reversed = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    return reversed;
+}
+
+// Test the function
+const input = "hello";
+const result = reverseString1(input);
+console.log(result);  // Output: "olleh"

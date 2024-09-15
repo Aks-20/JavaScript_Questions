76ff3a9251cf3aae94b67eb1ef9fc7f7168e9de0
Aks-20 <akshatgupta0043@gmail.com>
2024-09-15 22:48:03 +0530

    update

--- Arrays/classarray.js
+++ Arrays/classarray.js
@@ -1,13 +1,33 @@
-class MyArray{
-constructor()
-{
-    this.length=0;
-    this.data={}
+class MyArray {
+    constructor() {
+        this.length = 0;
+        this.data = {};
+    }
+
+    push(item) {
+        this.data[this.length] = item;
+        this.length++;
+        return this.length;
+    }
+
+    get(index) {
+        return this.data[index];
+    }
+
+
+
+    pop(){
+        const lastitem=this.data[this.length-1];
+        console.log(lastitem)
+    }
 }
-push (item){
-    this.data[this.length]=item
-    console.log("-----------",this.data)
-}
-}
-const newarray=new MyArray()
-console.log(newarray.push("apple"))
+
+const newArray = new MyArray();
+newArray.push("apple");
+newArray.push("pineapple");
+newArray.push("orange");
+
+console.log(newArray.get(1));  // Output: "pineapple"
+newArray.pop()
+
+console.log(newArray);  // View the entire custom array
